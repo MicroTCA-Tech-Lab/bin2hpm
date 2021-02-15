e19ddd82e0e7f91ddf49f61c385e01864d62b639
Patrick Huesmann <info@patrick-huesmann.de>
2021-02-15 12:19:08 +0100

    rle: Use bytestrings instead of arrays

--- bin2hpm/rle.py
+++ bin2hpm/rle.py
@@ -4,9 +4,10 @@ Copyright (c) 2021 Deutsches Elektronen-Synchrotron DESY.
 See LICENSE.txt for license details.
 """
 
-from array import array
-
 RLE_MAX_BLKSIZE=128
+
+def to_byte(b):
+    return bytes([b])
 
 def encode(data):
     def check_block(data):
@@ -18,9 +19,9 @@ def encode(data):
     print('Performing RLE compression ...')
     orig_len = len(data)
 
-    result = array('B')
+    result = b''
     while len(data):
-        tmp = array('B')
+        tmp = b''
         while len(data) and len(tmp) < RLE_MAX_BLKSIZE:
             n, val = check_block(data)
             if n > 2:
@@ -29,25 +30,22 @@ def encode(data):
             elif n == 2:
                 n = 1
             data = data[n:]
-            tmp.append(val)
+            tmp += to_byte(val)
         if len(tmp):
-            result.append(len(tmp) - 1)
-            result.extend(tmp)
+            result += to_byte(len(tmp) - 1)
+            result += tmp
         if n < 2:
             continue
-        result.append((n - 2) | 0x80)
-        result.append(val)
+        result += to_byte((n - 2) | 0x80)
+        result += to_byte(val)
     
     comp_len = len(result)
     comp_ratio = 100 * comp_len / orig_len
 
     print(f'Compressed size {comp_len} bytes, ratio {comp_ratio:.1f}%')
-    return bytes(result)
+    return result
 
 def decode(data):
-    def to_byte(b):
-        return b.to_bytes(1, 'little')
-
     result = b''
     while (len(data)):
         if data[0] & 0x80:
